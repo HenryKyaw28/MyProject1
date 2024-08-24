b15beeb18c5bc3356e10f47bf75eb3e38248b0a7
HenryKyaw <HenryKyaw28>
2024-08-24 22:07:10 +0630

    upper nav and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
       fontFamily: {
         main: ['new-amsterdam-regular', 'Nunito']
       },
+      colors: {
+        forText: {
+          dark: '#f2fed7',
+          light: '#d1dabc',
+        },
+        background: '#0e1300',
+        primary: '#dffd7e',
+        secondary: {
+          dark: '#029d03',
+          light: '#00cc02',
+        },
+        accent: '#29fc6e',
+      },
     },
   },
   plugins: [],
